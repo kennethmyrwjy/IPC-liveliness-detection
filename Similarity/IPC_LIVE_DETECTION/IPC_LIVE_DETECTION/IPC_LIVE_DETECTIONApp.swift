e79a8e3e730c31073878b3575769e601164e9053
LuisSsiuL <christian.luis.efendy@gmail.com>
2025-07-22 13:43:15 +0700

    test

---
+++ Similarity/IPC_LIVE_DETECTION/IPC_LIVE_DETECTION/IPC_LIVE_DETECTIONApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPC_LIVE_DETECTIONApp.swift
+//  IPC_LIVE_DETECTION
+//
+//  Created by Christian Luis Efendy on 21/07/25.
+//
+
+import SwiftUI
+
+@main
+struct IPC_LIVE_DETECTIONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
